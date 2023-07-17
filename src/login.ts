b8cd8aaba4f19388c595fb2753f0c1252b3731cd
nandobfer <nandobfer@yahoo.com.br>
2023-07-17 15:01:17 -0300

    update: login emitions

--- src/login.ts
+++ src/login.ts
@@ -5,11 +5,17 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 export const handleLogin = async (socket: Socket, clients: ClientBag, data: { login: string; password: string }) => {
-    const user = await prisma.user.findFirst({ where: { OR: [{ document: data.login }, { email: data.login }], AND: { password: data.password } } })
+    const user = await prisma.user.findFirst({
+        where: { OR: [{ document: data.login }, { email: data.login }], AND: { password: data.password } },
+        include: { crops: { include: { mediated: true } }, mediatedCrops: { include: { crop: { include: { producer: true } } } } },
+    })
 
     if (user) {
         clients.add({ socket, user })
         socket.emit("login:success", user)
+
+        const crops = await prisma.crop.findMany({ include: { producer: true, mediated: { include: { agent: true } } } })
+        socket.emit("crops:list", crops)
     } else {
         socket.emit("login:error")
     }
