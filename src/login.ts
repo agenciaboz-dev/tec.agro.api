de45aa5619dd937045292d48b6b31e7902977436
nandobfer <nandobfer@yahoo.com.br>
2023-07-18 10:54:40 -0300

    refac: prisma handling

--- src/login.ts
+++ src/login.ts
@@ -1,6 +1,7 @@
 import { Socket } from "socket.io"
 import { ClientBag } from "../definitions/client"
 import { PrismaClient } from "@prisma/client"
+import { fetch } from "./prisma"
 
 const prisma = new PrismaClient()
 
@@ -18,11 +19,8 @@ export const handleLogin = async (socket: Socket, clients: ClientBag, data: { lo
         clients.add({ socket, user })
         socket.emit("login:success", user)
 
-        const chats = await prisma.chat.findMany({ where: { users: { some: { id: user.id } } }, include: { messages: true, users: true } })
-        socket.emit("chats:list", chats)
-
-        const crops = await prisma.crop.findMany({ include: { producer: true, mediated: { include: { agent: true } } } })
-        socket.emit("crops:list", crops)
+        fetch.chats.user(user, (chats) => socket.emit("chats:list", chats))
+        fetch.crops.list((crops) => socket.emit("crops:list", crops))
     } else {
         socket.emit("login:error")
     }
