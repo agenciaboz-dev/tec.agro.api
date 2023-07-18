61cb731f7cbdac7d4758716370d4accc052b05fb
nandobfer <nandobfer@yahoo.com.br>
2023-07-18 16:29:44 -0300

    update: username into user signup

--- src/signup.ts
+++ src/signup.ts
@@ -5,12 +5,15 @@ import { PrismaClient, User } from "@prisma/client"
 const prisma = new PrismaClient()
 
 export const handleSignup = async (socket: Socket, data: User) => {
-    const user = await prisma.user.create({data: {
-        document: data.document,
-        email: data.email,
-        name: data.name,
-        password: data.password,
-    }})
+    const user = await prisma.user.create({
+        data: {
+            username: data.username,
+            document: data.document,
+            email: data.email,
+            name: data.name,
+            password: data.password,
+        },
+    })
 
     if(user){
         socket.emit("signup:success")
