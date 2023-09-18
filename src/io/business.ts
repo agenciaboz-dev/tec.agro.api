ee3a45785f923ad92eaf959f05b78a187a7414c3
nandobfer <nandobfer@yahoo.com.br>
2023-09-18 10:40:34 -0300

    update: broadcasting updated user on new business

--- src/io/business.ts
+++ src/io/business.ts
@@ -34,6 +34,7 @@ export const handleBusiness = async (socket: Socket, data: Business & { file: Ar
         const user = (await fetch.user.get(client.user.id)) as User
         console.log(user)
         socket.emit("user:update", user)
+        socket.broadcast.emit("user:update", user)
         clients.update(client, user)
     }
     
